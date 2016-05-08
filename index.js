ae6a6160f749d89ec449abe295db6cb8e7bfae82
Andrea Parodi <andrea@parro.it>
2016-05-08 08:38:55 +0200

    first release

--- index.js
+++ index.js
@@ -1,5 +1,55 @@
 'use strict';
 
-module.exports = function electronFileInput() {
-	return 42;
-};
+const {createElement} = require('react');
+const {remote} = require('electron');
+const context = require('electron-contextmenu-middleware');
+
+const inputs = new WeakMap();
+
+context.use(({elm, menu}, next) => {
+	if (inputs.has(elm)) {
+		menu.push({
+			label: 'Reset',
+			enabled: !elm.disabled,
+			click: () => {
+				inputs.get(elm).onChangeFile('');
+			}
+		});
+	}
+	next();
+});
+
+function FileInput(props) {
+	const onClick = e => {
+		if (e.target.disabled) {
+			return;
+		}
+
+		const file = remote.dialog.showOpenDialog({
+			properties: [
+				'openFile'
+				// ,'openDirectory', 'multiSelections'
+			]
+		});
+
+		if (file !== undefined) {
+			props.onChangeFile(file[0]);
+		}
+	};
+
+	const setInput = input => {
+		if (input !== null && input !== undefined) {
+			inputs.set(input, props);
+		}
+	};
+
+	return createElement('input', Object.assign({}, props, {
+		ref: setInput,
+		readOnly: true,
+		type: 'text',
+		style: {textOverflow: 'ellipsis'},
+		onClick: onClick
+	}));
+}
+
+module.exports = FileInput;
--- test.js
+++
@@ -1,7 +0,0 @@
-import test from 'ava';
-import electronFileInput from './';
-
-test('it work!', t => {
-	const result = electronFileInput();
-	t.is(result, 42);
-});
